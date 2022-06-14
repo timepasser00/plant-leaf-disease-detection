4c30badb013ff22efb6f8b5bf6dd0e55224d63a0
Atul Raj <atul03022019@gmail.com>
2022-06-14 13:43:38 +0530

    test

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def result():
         return render_template("index.html",data_param = data)
     return render_template("index.html")
     
-    
+#Test Deploy
 
 if __name__ == "__main__":
     app.run(debug = True)
